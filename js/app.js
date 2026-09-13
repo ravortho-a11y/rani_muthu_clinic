de43d5f72fbb0340432e813b53373c8fe059b855
ravortho-a11y <ravortho@gmail.com>
2026-09-13 11:17:53 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-g
+document.addEventListener('DOMContentLoaded', () => {
+  const menu = document.querySelector('[data-menu]');
+  const nav = document.querySelector('[data-nav]');
+  if (menu && nav) menu.addEventListener('click', () => nav.classList.toggle('open'));
+});
